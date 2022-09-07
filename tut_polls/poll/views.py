3048e68e6a7d3b474373dab15f8c54f67854afcc
yathish <yathishkumar.satheesh@softsuave.com>
2022-09-07 22:38:50 +0530

    intial html and routing and using context

--- tut_polls/poll/views.py
+++ tut_polls/poll/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse(request.path)
+    questions = Question.objects.order_by('pub_date')
+    template = loader.get_template('index.html')
+    context = {
+        'questions':questions
+    }
+    output = ','.join([question.question_text for question in questions])
+    return HttpResponse(template.render(context, request))
