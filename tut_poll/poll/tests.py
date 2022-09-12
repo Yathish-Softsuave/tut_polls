934dfcbd1bc7627cd7a5f72895fe08e1430429d6
yathish <yathishkumar.satheesh@softsuave.com>
2022-09-12 17:46:55 +0530

    add docstrings to testcases

--- tut_poll/poll/tests.py
+++ tut_poll/poll/tests.py
@@ -46,7 +46,7 @@ class QuestionIndexViewTest(TestCase):
 
     def test_no_questions(self):
         """
-          If no questions exist, an appropriate message is displayed.
+            If no questions exist, an appropriate message is displayed.
         """
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code, 200)
@@ -54,23 +54,38 @@ class QuestionIndexViewTest(TestCase):
         self.assertQuerysetEqual(response.context['questions'], [])
 
     def test_past_questions(self):
+        """
+            Questions with a pub_date in the past are displayed on the
+            index page.
+        """
         question = create_question('past question', days=-30)
         response = self.client.get(reverse('polls:index'))
         print(response.context['questions'])
         self.assertQuerysetEqual(response.context['questions'], [question])
 
     def test_future_questions(self):
+        """
+           Questions with a pub_date in the future aren't displayed on
+           the index page.
+        """
         create_question('future question', 30)
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(response.context['questions'], [])
 
     def test_past_and_future_questions(self):
+        """
+           Even if both past and future questions exist, only past questions
+           are displayed.
+        """
         past_question = create_question('past', -1)
         create_question('future', 1)
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(response.context['questions'], [past_question])
 
     def test_two_past_questions(self):
+        """
+            The questions index page may display multiple questions.
+        """
         question_one = create_question('first', -2)
         question_two = create_question('second', -2)
         response = self.client.get(reverse('polls:index'))
