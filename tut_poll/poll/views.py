4ccd097b2c5408d6dd51a65343f52eeb7c4bbb9b
yathish <yathishkumar.satheesh@softsuave.com>
2022-09-12 17:53:41 +0530

    Docstring for the views

--- tut_poll/poll/views.py
+++ tut_poll/poll/views.py
@@ -13,24 +13,45 @@ logger = logging.getLogger(__name__)
 
 
 class IndexView(generic.ListView):
+    """
+        view to show the list of all the questions present for the poll.
+    """
+
     template_name = 'index.html'
     context_object_name = 'questions'
 
     def get_queryset(self):
+        """
+            queryset for IndexView
+        :return:
+            questions which are published in the past
+        """
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
 
 
 class DetailView(generic.DetailView):
+    """
+        view to show the details and options for the questions to vote
+    """
     model = Question
     template_name = 'detail.html'
 
 
 class ResultsView(generic.DetailView):
+    """
+        view to show the results for the questions.
+    """
     model = Question
     template_name = 'result.html'
 
 
 def vote(request, question_id):
+    """
+    view to vote on the question
+    :param request: request body
+    :param question_id: pk of the question
+    :return: redirects to results
+    """
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
